ad2e8d11a6e0274300e88eb516637df7cee04fe7
Althafia Defiyandrea Laskanadya Wibowo <laskanadya@gmil.com>
2026-03-01 20:56:51 +0700

    Update home blade

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home - Sistem Posyandu</title>
+    <meta charset="UTF-8">
+</head>
+<body style="font-family: Arial; text-align: center; margin-top: 100px;">
+
+    <h1>Sistem Monitoring Pertumbuhan Balita</h1>
+
+    <p>
+        Website ini digunakan untuk memantau pertumbuhan balita 
+        dan mendeteksi risiko stunting.
+    </p>
+
+    <br>
+
+    <a href="/login">
+        <button style="padding:10px 20px; font-size:16px;">
+            Login
+        </button>
+    </a>
+
+</body>
+</html>
